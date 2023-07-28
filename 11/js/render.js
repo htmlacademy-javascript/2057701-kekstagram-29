07a3b5b29b2391cad1e8f30afa7a22ee24d5e987
Keks <keks@htmlacademy.ru>
2023-07-29 00:35:17 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/render.js
@@ -0,0 +1,40 @@
+import {renderBigPicture} from './full-picture-render.js';
+import {getData} from './server-api.js';
+import {showRenderErrorMessage} from './messages.js';
+import {showFilter} from './content-filters.js';
+import {debounce} from './utils.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureContainer = document.querySelector('.pictures');
+const picturesContainerFragment = document.createDocumentFragment();
+
+const renderMiniatures = (photosWithDescriptions) => {
+  photosWithDescriptions.forEach((photo) => {
+    const pictureContainerItem = pictureTemplate.cloneNode(true);
+
+    pictureContainerItem.querySelector('.picture__img').src = photo.url;
+    pictureContainerItem.querySelector('.picture__comments').textContent = photo.comments.length;
+    pictureContainerItem.querySelector('.picture__likes').textContent = photo.likes;
+
+    pictureContainerItem.addEventListener('click', (evt) => {
+      renderBigPicture(evt, photo);
+    });
+
+    picturesContainerFragment.append(pictureContainerItem);
+  });
+
+  pictureContainer.append(picturesContainerFragment);
+};
+
+const renderPictures = () => {
+  getData()
+    .then((photosWithDescriptions) => {
+      renderMiniatures(photosWithDescriptions);
+      showFilter(photosWithDescriptions, debounce(renderMiniatures));
+    })
+    .catch(() => {
+      showRenderErrorMessage();
+    });
+};
+
+export {renderPictures, renderMiniatures};
