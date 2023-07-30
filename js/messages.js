e33aa7be6f9ee24c58161788009cbd1ebbb5927e
Александр <badzop@gmail.com>
2023-07-30 19:12:04 +0500

    fix naming

--- js/messages.js
+++ js/messages.js
@@ -1,6 +1,7 @@
 import {addStyleToElement, isEscapeKeydown, stopPropagation} from './utils.js';
+import {onUploadModalKeydown} from './upload-form.js';
 
-const postSuccesTemplate = document.querySelector('#success').content.querySelector('.success');
+const postSuccessTemplate = document.querySelector('#success').content.querySelector('.success');
 const postErrorTemplate = document.querySelector('#error').content.querySelector('.error');
 const renderErrorStyle = {
   'z-index': '100',
@@ -22,12 +23,17 @@ const showRenderErrorMessage = () => {
   document.body.append(alertContainer);
 };
 
-const addOnCloseListeners = (message, messageButton, messageSelector) => {
+const addOnCloseListeners = (message, messageButton, messageSelector, isErrorMessage = false) => {
 
   const closePostMessage = () => {
     document.removeEventListener('keydown', onDocumentKeydown);
     document.removeEventListener('click', closePostMessage);
     document.body.removeChild(message);
+
+    if (isErrorMessage) {
+      document.addEventListener('keydown', onUploadModalKeydown);
+    }
+
   };
 
   function onDocumentKeydown (evt) {
@@ -46,7 +52,7 @@ const addOnCloseListeners = (message, messageButton, messageSelector) => {
 
 const showPostSuccessMessage = () => {
 
-  const successMessage = postSuccesTemplate.cloneNode(true);
+  const successMessage = postSuccessTemplate.cloneNode(true);
   const successButton = successMessage.querySelector('.success__button');
   const successSelector = '.success__inner';
 
@@ -55,11 +61,13 @@ const showPostSuccessMessage = () => {
 
 const showPostErrorMessage = () => {
 
+  document.removeEventListener('keydown', onUploadModalKeydown);
+
   const errorMessage = postErrorTemplate.cloneNode(true);
   const errorButton = errorMessage.querySelector('.error__button');
   const errorSelector = '.error__inner';
 
-  addOnCloseListeners(errorMessage, errorButton, errorSelector);
+  addOnCloseListeners(errorMessage, errorButton, errorSelector, true);
 };
 
 export {showRenderErrorMessage, showPostSuccessMessage, showPostErrorMessage};
--- js/validation.js
+++ js/validation.js
@@ -2,11 +2,12 @@ import {showPostErrorMessage, showPostSuccessMessage} from './messages.js';
 import {postData} from './server-api.js';
 import {closeModal} from './upload-form.js';
 
+const MAX_NUMBER_OF_HASHTAGS = 5;
+
 const uploadForm = document.querySelector('.img-upload__form');
 const submitButton = uploadForm.querySelector('#upload-submit');
 const hashtagInput = uploadForm.querySelector('input[name="hashtags"]');
-const regex = /^#[a-zа-я0-9]{1,19}$/;
-const MAX_NUMBER_OF_HASHTAGS = 5;
+const REGEX = /^#[a-zа-я0-9]{1,19}$/;
 
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__text',
@@ -16,15 +17,18 @@ const pristine = new Pristine(uploadForm, {
 
 const validateHashtags = () => {
   const hashtagsText = hashtagInput.value.trim().toLowerCase();
+  submitButton.disabled = false;
   if (hashtagsText === '') {
     return true;
   }
   const hashtags = hashtagsText.split(/\s+/);
   if (hashtags.length > MAX_NUMBER_OF_HASHTAGS || hashtags.length !== new Set(hashtags).size) {
+    submitButton.disabled = true;
     return false;
   }
   for (const hashtag of hashtags) {
-    if (!hashtag.match(regex)) {
+    if (!hashtag.match(REGEX)) {
+      submitButton.disabled = true;
       return false;
     }
   }
@@ -45,7 +49,6 @@ uploadForm.addEventListener('submit', (evt) => {
         showPostSuccessMessage();
       })
       .catch(() => {
-        closeModal(true);
         showPostErrorMessage();
       })
       .finally(() => {
